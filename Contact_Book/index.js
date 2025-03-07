9bebd7c6c7b56a255e36cf07a50b9b155ab03f9f
ElenaGQA <elena.grebeniuk@gmail.com>
2025-03-07 10:57:05 -0500

    update

--- Contact_Book/index.js
+++ Contact_Book/index.js
@@ -15,7 +15,7 @@ let newFileRoot = path.join(CurrentDirectory, "contacts.json")
 // })
 
 function addContact(contactName, contactPhone, contactEmail) {
-    fs.promises.readFile(newFileRoot, "utf-8")
+    return fs.promises.readFile(newFileRoot, "utf-8")
         .then((data) => {
             return JSON.parse(data || [])
         })
@@ -28,10 +28,10 @@ function addContact(contactName, contactPhone, contactEmail) {
                 email: contactEmail
             }
             contacts.push(newContact)
-          
-            fs.promises.writeFile(newFileRoot, JSON.stringify(contacts))
+
+            return fs.promises.writeFile(newFileRoot, JSON.stringify(contacts, null, 2))
                 .then(() => {
-                    console.log("file is updated")
+                    console.log("File is updated")
                 })
                 .catch((err) => {
                     console.log(err.message)
@@ -41,7 +41,57 @@ function addContact(contactName, contactPhone, contactEmail) {
 
 }
 
-addContact("Anna", "123", "a@gmail.com")
+// addContact("Anna", 123, "agmail.com")
+//     .then(() => {
+//         addContact("John", 456, "bgmail.com")
+//     })
 
-// viewContacts();
-// deleteContact(id);
+function viewContacts() {
+    fs.promises.readFile(newFileRoot, "utf-8")
+        .then((data) => {
+            return JSON.parse(data || [])
+        })
+        .catch(() => [])
+        .then((data) => {
+            if (data.length == 0) {
+                console.log("There is no contacts")
+            }
+            else {
+                data.forEach((element) => {
+                    console.log(`The name is ${element.name}, the phone number: ${element.phone}, eamil: ${element.email}`)
+                })
+            }
+        })
+}
+
+viewContacts()
+
+function deleteContact(id) {
+    fs.promises.readFile(newFileRoot, "utf-8")
+        .then((data) => {
+            return JSON.parse(data || [])
+        })
+        .catch(() => [])
+        .then((data) => {
+            if (data.length == 0) {
+                console.log("There is no contacts")
+            }
+            else {
+                let updatedData = data.filter((data) => {
+                    return data.id !== id
+                })
+                console.log(updatedData)
+                fs.promises.writeFile(newFileRoot, JSON.stringify(updatedData, null, 2))
+                    .then(() => {
+                        console.log("file is updated")
+                    })
+                    .catch((err) => {
+                        console.log(err.message)
+                    })
+
+            }
+
+        })
+}
+
+deleteContact(1741362722992);
