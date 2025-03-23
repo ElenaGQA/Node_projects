f46f5018fb4ae5804387312a277097cabe9a9ac7
ElenaGQA <elena.grebeniuk@gmail.com>
2025-03-23 19:59:58 -0400

    update

--- Blog_CMS/public/script.js
+++ Blog_CMS/public/script.js
@@ -1,5 +1,3 @@
-
-
 let postList = document.querySelector("#postList");
 let postForm = document.querySelector("#postForm");
 let title = document.querySelector("#title");
@@ -7,7 +5,7 @@ let content = document.querySelector("#content");
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
-
+    displayPosts()
     postForm.addEventListener("submit", (event) => {
         event.preventDefault();
         let titleValue = title.value;
@@ -18,55 +16,62 @@ document.addEventListener("DOMContentLoaded", (event) => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(titleValue, contentValue)
+            body: JSON.stringify({ title: titleValue, content: contentValue })
         })
-            .then(res => {
-                res.json();
-            })
+            .then(res => res.json())
             .then(res => {
                 titleValue = "";
                 contentValue = "";
+                location.reload(); //displayPosts();
             })
             .catch(err => {
                 console.log(err.message);
             })
 
-        function displayPosts() {
-            fetch('/posts', {
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(res => res.json())
-                .then(data => {
-                    postList.innerHTML = ""; // Clear existing list
-
-                    data.forEach(post => {
-                        const li = document.createElement("li"); 
-                        li.textContent = `<p>${post.title}</p><p>${post.content}</p> <button>Delete</button>`; 
-                        postList.appendChild(li); 
-                    });
-
-                })
-                .catch(err => {
-                    console.log(err.message);
-                });
-        }
-        // have fetch with method get (/posts). We need to show those posts in postList ul in this structure
-
-        // <li> <p>title</p> <p>content</p> <button>Delete</button> </li>
     })
+})
+
+function displayPosts() {
+    fetch('/posts', {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json' // ????
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            postList.innerHTML = "";  // Clear existing list
+
+            data.forEach(post => {
+                const li = document.createElement("li");
+                li.textContent = `<p>${post.title}</p><p>${post.content}</p> <button class = "deleteButton" data-id ="${post.id}" >Delete</button>`;
+                postList.appendChild(li);
+                li.querySelector(".deleteButton").addEventListener('click', () => { //skip for later
+                    deletePost(li.querySelector(".deleteButton").getAttribute('data-id'));
+                })
+            });
+
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+}
 
 
-
-
-
-
-
-
-
-
-
-});
-
+function deletePost(id) {
+    if (confirm("Are you sure you want to delete?")) {
+        fetch(`/delete?id=${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "text/plain"
+            }
+        })
+            .then(res => res.text())
+            .then(() => {
+                location.reload();
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    }
+}
--- Blog_CMS/server.js
+++ Blog_CMS/server.js
@@ -1,11 +1,19 @@
 import path from "path";
 import fs from "fs";
 
+let mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript"
+}
+
 let currentDir = path.resolve();
 let htmlPath = path.join(currentDir, `public/index.html`);
 
 const server = http.createServer((req, res) => {
-    fs.createReadStream(htmlPath).pipe(res)
+    fileSender(req, res, ()=>{
+        fs.createReadStream(htmlPath).pipe(res)
+    }) 
     req.on("data", chunk => {
         console.log(`Received Chunk: ${chunk}`);
         res.writeHead(200, { "Content-Type": "text/plain" });
@@ -16,6 +24,18 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Data received successfully.")
     })
+    
 })
 
+function fileSender(req, res, next){
+    let filePath = path.join(currentDir, `public/${req.url}`);
+    fs.promises.access(filePath)
+    .then((result)=>{
+        fs.createReadStream(filePath).pipe(res)
+    })
+    .catch(()=> next())
+}
+
 server.listen(3001);
+
+// if??? 
